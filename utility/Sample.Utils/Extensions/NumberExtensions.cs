c344faee8910ab94eb194d6c25aa4175468926ff
João Mesquita <joao.mesquita@atlasinovacoes.com.br>
2025-05-31 14:29:46 -0300

    🔪split and create submodule to sample utils

--- utility/Sample.Utils/Extensions/NumberExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Sample.Utils.Extensions;
-
-public static class NumberExtensions
-{
-    public static bool IsBetween<T>(this T value, T min, T max) where T : IComparable<T>
-        => (min.CompareTo(value) <= 0) && (value.CompareTo(max) <= 0);
-}
